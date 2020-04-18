942fe41531dac63b43591c10424b1e588c927019
atul.k <atul.k@flipkart.com>
2020-04-18 16:02:02 +0530

    Add training feature

---
+++ src/main/java/com/cityfox/user/dao/TrainingRepository.java
@@ -0,0 +1,8 @@
+package com.cityfox.user.dao;
+
+/**
+ * @author Atul Kumar , Date : 18/04/20
+ */
+public class TrainingRepository {
+
+}
---
+++ src/main/java/com/cityfox/user/service/TrainingService.java
@@ -0,0 +1,8 @@
+package com.cityfox.user.service;
+
+/**
+ * @author Atul Kumar , Date : 18/04/20
+ */
+public class TrainingService {
+
+}
